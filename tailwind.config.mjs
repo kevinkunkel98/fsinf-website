5d753d9938484f14645b58c68457f465b895eee4
kevinkunkel98 <kevinkunkel98@gmail.com>
2025-01-25 11:58:16 +0100

    Add Flowbite Accordion and Carousel

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	darkMode: 'class',
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite/**/*.js',
+	],
 	theme: {
 		extend: {
 			transformOrigin: {
@@ -9,5 +12,7 @@ export default {
 			},
 		},
 	},
-	plugins: [],
+	plugins: [
+		require('flowbite/plugin') // Add Flowbite plugin here
+	],
 };
