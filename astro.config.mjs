0f186a36c809766f54e6ed4cf1ef614e6487c5f3
Lovis Kilian Rentsch <vl44holo@studserv.uni-leipzig.de>
2025-09-19 21:25:48 +0000

    ci: fix missing static files
    For some reason our static rendered content is saved in dist instead of public.
    
    Move the previous public directory to something else and move dist
    to public to get it running in GitLab Pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://fsinf.informatik.uni-leipzig.de',
   integrations: [
     mdx(),
     sitemap(),
